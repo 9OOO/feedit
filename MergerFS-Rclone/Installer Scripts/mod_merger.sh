71f0e07c5a634ee2bfac2b61645502901ce30942
9OOO <63834092+9OOO@users.noreply.github.com>
2020-04-29 01:30:46 +0700

    Create mod_merger.sh
    /bin/ replaced with /sbin/ 
    only

---
+++ MergerFS-Rclone/Installer Scripts/mod_merger.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# MergerFS 2.28.3 Installer/Updater by Xan#7777
+
+if pgrep "mergerfs";
+then
+    echo "mergerfs is running. Please close all mergerfs instances before proceeding."
+    exit
+else
+    echo "mergerfs is installing/upgrading..."
+    mkdir -p "$HOME"/tmp
+    wget https://github.com/trapexit/mergerfs/releases/download/2.28.3/mergerfs_2.28.3.debian-stretch_amd64.deb -O "$HOME"/tmp/mergerfs.deb
+    dpkg -x "$HOME"/tmp/mergerfs.deb "$HOME"/tmp
+    mv "$HOME"/tmp/usr/bin/* "$HOME"/sbin
+    rm -rf "$HOME"/tmp
+    command -v mergerfs
+    mergerfs -v
+    exit
+fi
